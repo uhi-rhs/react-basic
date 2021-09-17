9fb5707efc9870168488f2d25afefed3c26e399d
neil davidson <neil.j.davidson@gmail.com>
2021-09-17 18:01:57 +0100

    basic instructions minimise

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Instructions = ({instructions}) => {
     const {header, item1, item2, body} = instructions
+
+    const [display, setDisplay] = useState(true)
+
+    const handleClick = () => {
+        setDisplay(!display)
+    }
+
+    if (!display) {
     return (
         <div className="instructions">
             <div className="instructions-header">
-        <h2>{header}</h2>
+            <h2>{header}</h2>
+            </div>
+            <button onClick={handleClick}>^</button>
         </div>
-        <ul>
-            <li>
-                {item1}
-            </li>
-            <li>
-                {item2}
-            </li>
-        </ul>
-        <p>{body}</p>
-    </div>
-    )
+    )}else{
+        return (
+            <div className="instructions">
+                <div className="instructions-header">
+            <h2>{header}</h2>
+            </div>
+            <button onClick={handleClick}>^</button>
+            <ul>
+                <li>
+                    {item1}
+                </li>
+                <li>
+                    {item2}
+                </li>
+            </ul>
+            <p>{body}</p>
+        </div>
+        )
+    }
 }
 
 export default Instructions
