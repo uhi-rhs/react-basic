a61eb6094c20996224d6af26e0f2e98414c17c54
neil davidson <neil.j.davidson@gmail.com>
2022-03-10 12:12:26 +0000

    trigger redeploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,10 @@ import { SocialIcon } from 'react-social-icons'
 const Footer = () => {
   return (
     <div className="">
-    <SocialIcon url="#" className="ml-4 mt-2" target="_blank" fgColor="#fff" bgColor="#1e3a8a" style={{ height: 24, width: 24} }/>
-    <SocialIcon url="#" className="ml-4 mt-2" target="_blank" fgColor="#fff" bgColor="#1e3a8a" style={{ height: 24, width: 24} }/>
-    <SocialIcon url="#" className="ml-4 mt-2" target="_blank" fgColor="#fff" bgColor="#1e3a8a" style={{ height: 24, width: 24} }/>
+    <SocialIcon url="https://www.linkedin.com/company/rural-housing-scotland/" className="ml-4 mt-2" target="_blank" fgColor="#fff" bgColor="#1e3a8a" style={{ height: 24, width: 24} }/>
+    <SocialIcon url="https://www.facebook.com/RuralHousingScotland" className="ml-4 mt-2" target="_blank" fgColor="#fff" bgColor="#1e3a8a" style={{ height: 24, width: 24} }/>
+    <SocialIcon url="https://twitter.com/RuralHousingSco" className="ml-4 mt-2" target="_blank" fgColor="#fff" bgColor="#1e3a8a" style={{ height: 24, width: 24} }/>
+    <SocialIcon url="https://www.instagram.com/ruralhousingscotland/" className="ml-4 mt-2" target="_blank" fgColor="#fff" bgColor="#1e3a8a" style={{ height: 24, width: 24} }/>
     </div>
   )
 }
